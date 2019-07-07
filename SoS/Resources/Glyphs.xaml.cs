bf3612f7912b99c379739415453391868e5250cf
Joel Musheno <joelmusheno@gmail.com>
2019-07-07 08:57:28 -0400

    holding on the shared resources for a second. think i have a pattern

--- SoS/Resources/Glyphs.xaml.cs
+++ SoS/Resources/Glyphs.xaml.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using Xamarin.Forms;
+
+namespace SoS.Resources
+{
+    public partial class Glyphs : ResourceDictionary
+    {
+        public Glyphs()
+        {
+            InitializeComponent();
+        }
+    }
+}
